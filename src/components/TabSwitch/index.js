3f07bccd91792544c34dec0f66718907a0dcf70a
rajeshRoul <roul.rajesh28@gmail.com>
2022-10-22 22:20:04 +0530

    Create Tab Switcher Component

--- src/components/TabSwitch/index.js
+++ src/components/TabSwitch/index.js
@@ -1,8 +1,19 @@
 import classes from "./tabSwitch.module.scss";
 
-const TabSwitch = () => {
+const TabSwitch = ({ tabs, activeTab, onChange = () => { } }) => {
     return (
-        <div className={classes.container}></div>
+        <div className={classes.container}>
+            {
+                tabs.map((tab, index) => (
+                    <div
+                        key={`tab${index}${tab.value}`}
+                        onClick={() => onChange(tab)}
+                        className={`${classes.tab} ${activeTab?.value === tab.value ? classes.active : ""}`}>
+                        {tab.label}
+                    </div>
+                ))
+            }
+        </div>
     )
 }
 
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,7 +1,33 @@
 import TabSwitch from "components/TabSwitch";
+import { useState } from "react";
+import classes from "./homePage.module.scss";
 
 const HomePage = () => {
-    return <div><TabSwitch/></div>
+    const [activeTab, setActiveTab] = useState(tabs[0]);
+
+    return <div className={classes.container}>
+        <div className={classes.tabSwitchCtr}>
+            <TabSwitch
+                tabs={tabs}
+                activeTab={activeTab}
+                onChange={setActiveTab} />
+        </div>
+    </div>
 }
+
+const tabs = [
+    {
+        label: "Resources",
+        value: "Resources"
+    },
+    {
+        label: "Requests",
+        value: "Requests"
+    },
+    {
+        label: "Users",
+        value: "Users"
+    }
+]
 
 export default HomePage;
