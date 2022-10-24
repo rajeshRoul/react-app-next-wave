27dbd62b9e86b83e62028bba92b0affc2365b7b2
rajeshRoul <roul.rajesh28@gmail.com>
2022-10-25 00:30:35 +0530

    Make Header Component Fixed, Show User Profile Pic in the Header, Logout Button On Hover on profile pic at Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,44 @@
 import classes from "./header.module.scss";
 import NxtWaveLogo from "assets/icons/NxtWaveLogo.svg";
 import UserIcon from "assets/icons/UserIcon.svg";
+import { useSelector } from "react-redux";
+import DropdownButton from "components/DropdownButton";
+import AuthenticationAPI from "ServerConnect/AuthenticationAPI";
 
 const Header = () => {
+    const isLoggedIn = useSelector((store) => store?.user?.isLoggedIn);
+    const userData = useSelector((store) => store?.user?.data);
+
+    const logoutUser = () => {
+        AuthenticationAPI.logoutUser();
+    }
+
     return (
         <div className={classes.headerCtr}>
             <img src={NxtWaveLogo} alt="Next Wave" />
-            <img src={UserIcon} alt="" className={classes.userIcon}/>
+            {isLoggedIn &&
+                <DropdownButton
+                    onChange={logoutUser}
+                    menuItems={userActions}>
+                    <img
+                        src={userData?.image || UserIcon}
+                        alt=""
+                        className={classes.userIcon}
+                        onError={({ currentTarget }) => {
+                            currentTarget.onerror = null;
+                            currentTarget.src = UserIcon;
+                        }} />
+                </DropdownButton>
+            }
         </div>
     )
 }
+
+const userActions = [
+    {
+        label: "Log Out",
+        value: "logOut"
+    }
+]
 
 export default Header;
